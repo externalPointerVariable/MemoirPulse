d2f9f79dc56fe06441529b61792f8606b3989206
externalPointerVariable <vicky986847@gmail.com>
2025-03-07 16:15:15 +0000

    Implement routing and layout for authentication and post management; add AddPost, AllPosts, EditPost, Home, and Login pages

--- myApp/src/components/index.js
+++ myApp/src/components/index.js
@@ -8,7 +8,7 @@ import Input from "./Input";
 import Button from "./Button";
 import RTE from "./RTE";
 import Signup from "./Signup";
-import Login from "./Login";
+import Login from "./LogIn";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
